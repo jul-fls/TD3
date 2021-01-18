876bbcf63b672450e196ae3a38434e5e118a720f
Julien Flusin <julienflusin3@gmail.com>
2021-01-18 14:34:07 +0100

    Perfected

--- TD3/TD3.c
+++ TD3/TD3.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 void main() {
-	long n=0,sum=0,min=0,max=0;
+	long n = 0, sum = 0, min = 0, max = 0;
 	int nvalues[1000];
 	printf("Choisir une valeur pour n :");
 	scanf("%d", &n);
@@ -9,6 +9,10 @@ void main() {
 	for (int i = 0;i < n;i++) {
 		printf("Entrer la valeur numero %d :\n", i + 1);
 		scanf("%d", &nvalues[i]);
+		if (i == 0) {
+			min = nvalues[i];
+			max = nvalues[i];
+		}
 		sum += nvalues[i];
 		if (nvalues[i] < min) {
 			min = nvalues[i];
@@ -20,4 +24,5 @@ void main() {
 	for (int i = 0;i < n;i++) {
 		printf("%d ", nvalues[i]);
 	}
-	printf("\nSomme = %d\nMinimum = %d\nMaximum = %d\n",sum,min,max);
+	printf("\nSomme = %d\nMinimum = %d\nMaximum = %d\n", sum, min, max);
+}
