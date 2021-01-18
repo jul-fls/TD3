dc08c04a7ff9b456a98fd97912eb46a20ff8543f
Julien Flusin <julienflusin3@gmail.com>
2021-01-18 13:50:03 +0100

    Ask values

--- TD3/TD3.c
+++ TD3/TD3.c
@@ -1,5 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
-int n;
-printf("Choisir une valeur pour ")
-int nvalues[n];
+void main() {
+	int n;
+	int nvalues[1000];
+	printf("Choisir une valeur pour n :");
+	scanf("%d", &n);
+	printf("Vous avez choisi la valeur %d !\nAllons-y !\n", n);
+	for (int i = 0;i < n;i++) {
+		printf("Entrer la valeur numero %d :\n", i + 1);
+		scanf("%d", &nvalues[i]);
+	}
+	for (int i = 0;i < n;i++) {
+		printf("Valeur %d = %d\n", i+1, nvalues[i]);
+	}
+}
